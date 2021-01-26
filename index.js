079ad595781112571b80cce22b8ffe5b18f57165
BrendonLH <Brendonhampton1988@gmail.com>
2021-01-26 00:08:23 -0800

    fixing cors

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const io = require('socket.io')(process.env.PORT, {
   cors: {
     origin:'*',
     methods: ['GET', 'POST'],
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        // "Access-Control-Allow-Origin": "https://example.com",
+        // "Access-Control-Allow-Methods": "GET,POST",
+        // "Access-Control-Allow-Headers": "my-custom-header",
+        "Access-Control-Allow-Credentials": true
+      });
+      res.end();
+    }
   }
 });
 
